dbec22a98fec15bc5c0a60cbb0be6c90de00876c
Daniil Balabanov <spacewalrus@bk.ru>
2023-07-06 07:07:37 +0300

    change routing and add dotenv

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,9 +1,18 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+from dotenv import load_dotenv
 
 
 app = Flask(__name__)
+load_dotenv()
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
 
 
 @app.route("/")
-def hello_world():
-    return "<p> Hello, World!</p>"
+def get_url():
+    return render_template('index.html')
+
+
+@app.route("/urls")
+def index():
+    return "Здесь будет БАЗА"
