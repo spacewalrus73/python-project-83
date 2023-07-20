ac4eb0993bdd0e5088d01498f94fa8fda4b3d0a6
Daniil Balabanov <spacewalrus@bk.ru>
2023-07-20 18:21:41 +0300

    add new routes

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,6 +1,7 @@
 import os
+from datetime import date
 from dotenv import load_dotenv
-from page_analyzer import db_manager as db
+from page_analyzer import db_actions as db
 from page_analyzer.validator import validate
 from flask import (
     Flask,
@@ -12,7 +13,6 @@ from flask import (
 
 
 load_dotenv()
-DATABASE_URL = os.getenv('DATABASE_URL')
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
@@ -33,27 +33,60 @@ def receive_url():
             flash(error, "danger")
         return redirect(url_for('view_home_page', url=income_url), code=302)
 
-    with db.connect_db(DATABASE_URL) as conn:
+    id = db.select(table_name='urls',
+                   fields=['id'],
+                   where='name',
+                   param=income_url)
+    if id:
+        flash("Страница уже существует", "info")
+        return redirect(url_for('view_home_page', url=income_url), code=302)
 
-        if db.get_id(conn, income_url):
-            flash("Страница уже существует", "info")
-            return redirect(url_for('view_home_page', url=income_url), code=302)
+    db.insert(table_name='urls',
+              fields=['name', 'created_at'],
+              values=[income_url, date.today()])
 
-        db.add_url(conn, income_url)
-        id = db.get_id(conn, income_url)
+    id = db.select(table_name='urls',
+                   fields=['id'],
+                   where='name',
+                   param=income_url)
+    value_of_id = db.extract_one(id)
+
     flash("Страница добавлена успешно", "success")
-    return redirect(url_for("show_site_page", id=id), code=302)
+    return redirect(url_for("show_site_page", id=value_of_id), code=302)
 
 
 @app.get("/urls/<id>")
 def show_site_page(id):
-    with db.connect_db(DATABASE_URL) as conn:
-        item_data = db.get_by_id(conn, int(id))
-    return render_template('url_page.html', item=item_data)
+    item_data = db.select(table_name='urls',
+                          take_all=True,
+                          where='id',
+                          param=id)
+    item = db.change_structure(item_data, keys=["id", "name", "created_at"])[0]
+
+    checks_data = db.select(table_name='url_checks',
+                            take_all=True,
+                            where='url_id',
+                            param=id,
+                            sort_by='id')
+    checks = db.change_structure(checks_data,
+                                 keys=["id", "url_id", "status_code",
+                                       "h1", "title", "description",
+                                       "created_at"])
+
+    return render_template('url_page.html', item=item, checks=checks)
 
 
 @app.get("/urls")
 def show_websites():
-    with db.connect_db(DATABASE_URL) as conn:
-        data = db.get_all_data(conn)
+    selected = db.select_table_websites()
+    data = db.change_structure(selected, keys=["id", "name", "created_at"])
     return render_template("urls_page.html", data=data)
+
+
+@app.post("/urls/<id>/checks")
+def check(id):
+    db.insert(table_name='url_checks',
+              fields=['url_id', 'created_at'],
+              values=[id, date.today()])
+    flash("Страница успешно проверена", "success")
+    return redirect(url_for("show_site_page", id=id), code=302)
