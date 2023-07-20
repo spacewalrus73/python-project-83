fb48e3ee5463968f0776c2574c9cea68fead7066
Daniil Balabanov <spacewalrus@bk.ru>
2023-07-20 18:21:21 +0300

    add url_checks table

--- database.sql
+++ database.sql
@@ -2,3 +2,12 @@ CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255),
     created_at date);
+
+ CREATE TABLE url_checks (
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls (id),
+    status_code integer,
+    h1 text,
+    title varchar(255),
+    description text,
+    created_at date);
