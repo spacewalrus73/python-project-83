75a87abe349bfb7dc99a5c84456028df348da902
Daniil Balabanov <spacewalrus@bk.ru>
2023-08-04 00:04:44 +0300

    refacted to reflect the new module

--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,10 +1,9 @@
 import os
-from datetime import date
 from dotenv import load_dotenv
-from page_analyzer import db_actions as db
+import page_analyzer.actions as act
 from page_analyzer.validator import validate
 from page_analyzer.normalizer import normalize
-from page_analyzer.url_checker import get_seo_params, get_status_code
+from page_analyzer.url_checker import get_status_code
 from flask import (
     Flask,
     render_template,
@@ -36,82 +35,41 @@ def receive_url():
             flash(error, "danger")
         return render_template('home_page.html'), 422
 
-    id = db.select(table_name='urls',
-                   fields=['id'],
-                   where='name',
-                   param=normalized_url)
-    if id:
-        value_of_id = db.extract_one(id)
+    if act.is_exist(where_name=normalized_url):
         flash("Страница уже существует", "info")
-        return redirect(url_for('show_site_page', id=value_of_id), code=302)
+    else:
+        act.add_url(normalized_url)
+        flash("Страница успешно добавлена", "success")
 
-    db.insert(table_name='urls',
-              fields=['name', 'created_at'],
-              values=[normalized_url, date.today()])
-
-    id = db.select(table_name='urls',
-                   fields=['id'],
-                   where='name',
-                   param=normalized_url)
-    value_of_id = db.extract_one(id)
-
-    flash("Страница успешно добавлена", "success")
-    return redirect(url_for("show_site_page", id=value_of_id), code=302)
+    return redirect(
+        url_for("show_site_page", id=act.get_id(normalized_url)), code=302
+    )
 
 
 @app.get("/urls/<id>")
 def show_site_page(id):
-    item_data = db.select(table_name='urls',
-                          take_all=True,
-                          where='id',
-                          param=id)
-    item = db.change_structure(item_data, keys=["id", "name", "created_at"])[0]
-
-    checks_data = db.select(table_name='url_checks',
-                            take_all=True,
-                            where='url_id',
-                            param=id,
-                            sort_by='id')
-    checks = db.change_structure(checks_data,
-                                 keys=[
-                                     "id", "url_id", "status_code",
-                                     "h1", "title", "description",
-                                     "created_at"
-                                 ])
-
-    return render_template('url_page.html', item=item, checks=checks)
+    site = act.get_site(int(id))
+    checks = act.get_check(int(id))
+    return render_template('url_page.html', item=site, checks=checks)
 
 
 @app.get("/urls")
 def show_websites():
-    selected = db.select_table_websites()
-    data = db.change_structure(selected, keys=["id",
-                                               "name",
-                                               "created_at",
-                                               "status_code"])
-    return render_template("urls_page.html", data=data)
+    websites_data = act.get_websites()
+    return render_template("urls_page.html", data=websites_data)
 
 
 @app.post("/urls/<id>/checks")
 def check(id):
-    selected_url = db.select(table_name='urls',
-                             fields=["name"],
-                             where='id',
-                             param=int(id))
-    url = db.extract_one(selected_url)
+    url = act.get_url(id)
     code = get_status_code(url)
 
     if not code:
         flash("Произошла ошибка при проверке", "danger")
-        return redirect(url_for('show_site_page', id=id), code=302)
+    else:
+        act.add_check(id, code, url)
+        flash("Страница успешно проверена", "success")
 
-    seo_params = get_seo_params(url)
-    db.insert(table_name='url_checks',
-              fields=['url_id', 'created_at', 'status_code',
-                      'h1', 'title', 'description'],
-              values=[id, date.today(), code, seo_params["h1"],
-                      seo_params["title"], seo_params["description"]])
-    flash("Страница успешно проверена", "success")
     return redirect(url_for("show_site_page", id=id), code=302)
 
 
